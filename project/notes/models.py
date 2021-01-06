481827521c02e99ec931231b586523b229f81e60
Luka Isailovic <lukaisailovic@gmail.com>
2021-01-06 16:14:02 +0100

    Add user table as owner for label and notes

--- project/notes/models.py
+++ project/notes/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Label(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
 
 class Note(models.Model):
@@ -14,3 +16,5 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     label = models.ForeignKey(Label, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
