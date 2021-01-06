20eb386dbe40ab377022b0327ba4a22298ce4775
Luka Isailovic <lukaisailovic@gmail.com>
2021-01-06 16:06:29 +0100

    Master and registration templates

--- project/project/settings.py
+++ project/project/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+LOGIN_REDIRECT_URL = '/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'notes/templates')
+            'templates', os.path.join(BASE_DIR, 'notes/templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
