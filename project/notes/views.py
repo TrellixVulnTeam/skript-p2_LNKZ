9a76c7e7f1c865b6fd30300021ba702f74d8e9f5
Luka Isailovic <lukaisailovic@gmail.com>
2021-01-06 17:19:40 +0100

    Notes and labels display

--- project/notes/views.py
+++ project/notes/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Note, Label
+from django.contrib.auth.decorators import login_required, permission_required
 
 
-# Create your views here.
+@login_required
 def index(request):
-    return render(request, 'notes/index.html')
+    tmp_notes = Note.objects.filter(owner=request.user)
+    labels = Label.objects.filter(owner=request.user)
+    pinned = [x for x in tmp_notes if x.pinned]
+    notes = set(tmp_notes) - set(pinned)
+    return render(request, 'notes/index.html', {'notes': notes, 'labels': labels, 'pinned': pinned})
