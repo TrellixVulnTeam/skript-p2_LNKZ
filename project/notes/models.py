221c4ba6dd352700fd1dc630af9484536d00693b
Luka Isailovic <lukaisailovic@gmail.com>
2021-01-06 14:51:23 +0100

    Database setup

--- project/notes/models.py
+++ project/notes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Label(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=510)
+    pinned = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    label = models.ForeignKey(Label, on_delete=models.SET_NULL, blank=True, null=True)
