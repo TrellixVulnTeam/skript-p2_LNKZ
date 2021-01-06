1d89dc79a6b2daa14e160407509fa3016c4910f1
Luka Isailovic <lukaisailovic@gmail.com>
2021-01-06 18:20:15 +0100

    Delete note

--- project/notes/urls.py
+++ project/notes/urls.py
@@ -6,4 +6,5 @@ app_name = 'notes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('notes/<int:id>', views.notes_edit, name='edit'),
+    path('notes/delete/<int:id>', views.notes_delete, name='delete'),
 ]
